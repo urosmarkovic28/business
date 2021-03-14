eca3a36efe41f629de2b89cc215aba1676134989
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-14 13:48:15 +0100

    Headings html,css,responsive

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
         <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
         <button>About us</button>
     </header>
+    <!--Headings-->
+    <section class="container headings">
+        <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2><span>We can</span> help your buisness grow</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
     
 <?php include "partials/footer.php"?>
