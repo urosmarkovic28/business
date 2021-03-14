fee538a075380153a5b1c9aa7816cba0271d7ce9
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-14 13:32:14 +0100

    Header html,css,responsive

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include "partials/header.php"?>
     
-    <h1>Sutra content</h1>
+    <!--Header-->
+    <header>
+        <h1>Trusted and <br> professionals advisers</h1>
+        <h3>For your buisness</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+        <button>About us</button>
+    </header>
     
 <?php include "partials/footer.php"?>
