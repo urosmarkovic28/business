3219538ea760e4db38825be3df04562fffbe9071
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-14 12:46:52 +0100

    Footer html and css

---
+++ partials/footer.php
@@ -0,0 +1,23 @@
+<!--Footer-->
+<footer>
+        <div class="container">
+            <div class="col"><a href="index.php" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a></div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+            <i class="fas fa-envelope"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+            <i class="fas fa-phone"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
