678636b5eec870fb40dc5c020f907a4aa0c6c477
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-14 12:30:32 +0100

    Nav html and scss

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Business</title>
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
+     <link rel="stylesheet" href="scss/style.css">
+</head>
+<body>
+    <!--Nav-->
+    <nav>
+        <a href="index.php" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Team</a></li>
+            <li><a href="">Sign up</a></li>
+            <li><a href=""><i class="fas fa-search"></i></a></li>
+        </ul>
+    </nav>
+</body>
+</html>
